2bdb42bb04d1b87c6fe6be7c46023e1b0429b6c0
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-04-17 14:15:57 +0300

    Add files via upload

---
+++ Homework_3_and_8/Homework_3/Circle.h
@@ -0,0 +1,30 @@
+#ifndef Circle_H
+#define Circle_H
+
+#include <iostream>
+
+using namespace std;
+
+class Circle
+{
+private:
+
+    double r = 0;    
+
+    //Circle()     //конструктор
+    //~Circle();   //деструктор
+
+public:
+
+    double Set_r();
+
+    void Print_parametres();
+
+    double Get_diameter();
+
+    double Get_circumference_length();
+
+    double Get_area();
+};
+
+#endif
