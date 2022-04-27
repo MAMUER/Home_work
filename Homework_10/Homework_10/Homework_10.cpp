4e528865f9942c0efa92e5e308f0f3064df71df1
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-04-27 22:17:55 +0300

    Add files via upload

---
+++ Homework_10/Homework_10/Homework_10.cpp
@@ -0,0 +1,17 @@
+﻿#include "Human.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	Student a("Михайлов", "Пётр", "Сергеевич", 17, true);
+
+	Boss b("Cергеев", "Алексей", "Михайлович", 37, 8);
+
+	a.print();
+	b.print();
+
+	return 0;
+}
---
+++ Homework_10/Homework_10/Human.h
@@ -0,0 +1,98 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Human
+{
+private:
+	string surname;
+	string name;
+	string midname;
+	int age;
+
+public:
+	Human()
+	{
+		this->surname = "";
+		this->name = "";
+		this->midname = "";
+		this->age = 0;
+	}
+
+	Human(string a, string b, string c, int d)
+	{
+		this->surname = a;
+		this->name = b;
+		this->midname = c;
+		this->age = d;
+	}
+
+	string Get_surname()
+	{
+		return this->surname;
+	}
+
+	string Get_name()
+	{
+		return this->name;
+	}
+
+	string Get_midname()
+	{
+		return this->midname;
+	}
+
+	int Get_age()
+	{
+		return this->age;
+	}
+
+	virtual ~Human() = default;
+
+	virtual void print() = 0;
+};
+
+class Student :public Human
+{
+	bool on_lesson = false;
+public:
+	Student()
+	{
+		this->on_lesson = false;
+	}
+
+	Student(const string& a, const string& b, const string& c, const int& d, const bool& e) :Human(a, b, c, d)
+	{
+		this->on_lesson = e;
+	}
+
+	void print() override
+	{
+		cout << this->Get_surname() << " " << this->Get_name() << " " << this->Get_midname() << " " << this->Get_age() << " " << this->on_lesson << endl;
+	}
+};
+
+class Boss :public Human
+{
+	int number_of_workers;
+public:
+	Boss()
+	{
+		this->number_of_workers = 0;
+	}
+
+	Boss(const string& a, const string& b, const string& c, const int& d, const int& e) :Human(a, b, c, d)
+	{
+		this->number_of_workers = e;
+	}
+
+	void print() override
+	{
+		cout << this->Get_surname() << " " << this->Get_name() << " " << this->Get_midname() << " " << this->Get_age() << " " << this->number_of_workers << endl;
+	}
+};
+#endif
