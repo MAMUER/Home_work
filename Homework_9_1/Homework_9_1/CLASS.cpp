fb033f677e7bac52c52aaa3edc02a97d82610f40
MAMUER <101267610+MAMUER@users.noreply.github.com>
2022-05-06 12:50:49 +0300

    Update CLASS.cpp

--- Homework_9_1/Homework_9_1/CLASS.cpp
+++ Homework_9_1/Homework_9_1/CLASS.cpp
@@ -8,29 +8,29 @@ int main()
 
 	Property* arr[7];
 
-	double A = rand() % 20'000'000 + 100000;
-	
-	arr[0] = new Apartment(A);
-	A = rand() % 20'000'000 + 100000;
+	double A = rand() % 20'000'000 + 100'000;
+
+	arr[0] = new Apartment(A);	
+	A = rand() % 20'000'000 + 100'000;
 	arr[1] = new Apartment(A);
+	A = rand() % 20'000'000 + 100'000;
 	arr[2] = new Apartment(A);
+	A = rand() % 20'000'000 + 100'000;
 	arr[3] = new Car(A);
+	A = rand() % 20'000'000 + 100'000;
 	arr[4] = new Car(A);
+	A = rand() % 20'000'000 + 100'000;
 	arr[5] = new CountryHouse(A);
+	A = rand() % 20'000'000 + 100'000;
 	arr[6] = new CountryHouse(A);
 
-	cout << "Налог = " << a.tax() << endl;
+	for (int i = 0; i < 7; i++)
+	{
+		cout << "Налог = " << arr[i]->tax() << endl;
+	}	
 
-	cout << "Налог = " << b.tax() << endl;
-
-	cout << "Налог = " << c.tax() << endl;
-
-	cout << "Налог = " << d.tax() << endl;
-
-	cout << "Налог = " << e.tax() << endl;
-
-	cout << "Налог = " << f.tax() << endl;
-
-	cout << "Налог = " << g.tax() << endl;
-
+	for (int i = 0; i < 7; i++)
+	{
+		delete arr[i];
+	}
 }
